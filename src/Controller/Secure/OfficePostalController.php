bf4d0951a8678e20a90c87a911ba0b191d792549
Diego Vidal Caro <dvidalcaro@hotmail.com>
2024-08-22 13:56:22 -0300

    Se agrega vista oficinas

--- src/Controller/Secure/OfficePostalController.php
+++ src/Controller/Secure/OfficePostalController.php
@@ -6,13 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/office-postal')]
+
 class OfficePostalController extends AbstractController
 {
-    #[Route('/secure/office/postal', name: 'app_secure_office_postal')]
-    public function index(): Response
+    #[Route('/{status}', name: 'app_secure_office_postal')]
+    public function index($status="office"): Response
     {
         return $this->render('secure/office_postal/index.html.twig', [
             'controller_name' => 'OfficePostalController',
+            'active' => 'office',
         ]);
     }
 }
